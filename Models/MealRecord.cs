6d83a6e568a8cb5edb469c466a554eaf9ac790b9
meetali2409 <rajputmeetali249@gmail.com>
2026-04-06 08:49:52 +0530

    fixed all

--- Models/MealRecord.cs
+++ Models/MealRecord.cs
@@ -3,22 +3,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MealManagement.Models
 {
-    public class MealRecord
+ 
+public class MealRecord
     {
-        [Key]
         public int Id { get; set; }
 
         public int EmployeeId { get; set; }
         public int MealTypeId { get; set; }
-
-        public int FoodId { get; set; }  
+        public int FoodId { get; set; }
 
         public DateTime MealDate { get; set; }
 
         public Employee? Employee { get; set; }
         public MealType? MealType { get; set; }
-
-        [ForeignKey("FoodId")]
-        public FoodItem? FoodItem { get; set; } 
+        public FoodItem? FoodItem { get; set; }
     }
 }
